fb46bea5cbb9a0f1e327638cbbb2f9f0287b256b
roiacult <roiacult27@gmail.com>
2020-04-26 16:51:06 +0000

    implement presentation layer for login fragment

--- app/src/main/java/com/roacult/madinatic/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/roacult/madinatic/ui/auth/LoginFragment.kt
@@ -1,5 +1,87 @@
 package com.roacult.madinatic.ui.auth
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
+import com.roacult.kero.team7.jstarter_domain.interactors.None
+import com.roacult.madinatic.R
 import com.roacult.madinatic.base.FullScreenFragment
+import com.roacult.madinatic.databinding.LoginBinding
+import com.roacult.madinatic.databinding.ResetPasswordBinding
+import com.roacult.madinatic.utils.states.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class LoginFragment
+class LoginFragment : FullScreenFragment<LoginBinding>() {
+    override val layutIdRes = R.layout.login
+
+    private val viewModel : LoginViewModel by viewModel()
+
+    private lateinit var dialogue : AlertDialog
+    private lateinit var resetPasswordBinding : ResetPasswordBinding
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+        viewModel.observe(this){
+            it.errorMsg?.getContentIfNotHandled()?.apply {
+                onError(this)
+            }
+            it.showMsg?.getContentIfNotHandled()?.apply {
+                showMessage(this)
+            }
+            it.login?.apply { handleLoginState(this) }
+            it.resetPassword?.apply { handleResetPassword(this) }
+        }
+    }
+
+    private fun initViews() {
+        binding.viewModel = viewModel
+    }
+
+    private fun handleLoginState(async: Async<None>) {
+        //TODO change it later
+        when (async) {
+            is Loading -> binding.state = ViewState.LOADING
+            is Fail<*, *> -> binding.state = ViewState.EMPTY
+            is Success -> {
+                //TODO go to main page here
+                showMessage("TODO go to main here")
+            }
+        }
+    }
+
+    private fun forgetPasswordDialogue() {
+        dialogue = AlertDialog.Builder(context!!).apply {
+            resetPasswordBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(context),
+                R.layout.reset_password,
+                null,
+                false
+            )
+            setView(resetPasswordBinding.root)
+            resetPasswordBinding.viewModel = viewModel
+        }.create()
+        dialogue.show()
+    }
+
+
+    private fun handleResetPassword(async: Async<None>) {
+        when(async){
+            is Success -> {
+                dialogue.hide()
+                showMessage(getString(R.string.resetpassword_succ))
+                viewModel.finishResetPassword()
+            }
+            is Loading -> {
+                resetPasswordBinding.state = ViewState.LOADING
+            }
+            is Fail<*,*> -> {
+                resetPasswordBinding.state = ViewState.EMPTY
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/roacult/madinatic/ui/auth/LoginViewModel.kt
@@ -0,0 +1,108 @@
+package com.roacult.madinatic.ui.auth
+
+import com.roacult.domain.exceptions.AuthFailure
+import com.roacult.domain.usecases.auth.Login
+import com.roacult.domain.usecases.auth.LoginParams
+import com.roacult.domain.usecases.auth.ResetPassword
+import com.roacult.kero.team7.jstarter_domain.interactors.None
+import com.roacult.kero.team7.jstarter_domain.interactors.launchInteractor
+import com.roacult.madinatic.R
+import com.roacult.madinatic.base.BaseViewModel
+import com.roacult.madinatic.base.State
+import com.roacult.madinatic.utils.StringProvider
+import com.roacult.madinatic.utils.extensions.isValidEmail
+import com.roacult.madinatic.utils.states.*
+import timber.log.Timber
+
+class LoginViewModel(
+    private val login : Login,
+    private val resetPassword : ResetPassword,
+    private val stringProvider: StringProvider
+) : BaseViewModel<LoginState>(LoginState()){
+
+
+
+    fun login(email:String, password:String){
+
+        if(state.value!!.login is Loading){
+            setState{copy(errorMsg=Event(stringProvider.getStringFromResource(R.string.plz_patient)))}
+            return
+        }
+
+        if(!email.isValidEmail()){
+            setState { copy(errorMsg=Event(stringProvider.getStringFromResource(R.string.invalid_email))) }
+            return
+        }
+
+        if(password.length<8){
+            setState { copy(errorMsg = Event(stringProvider.getStringFromResource(R.string.invalid_password))) }
+            return
+        }
+
+        setState { copy(login=Loading()) }
+        scope.launchInteractor(login, LoginParams(email, password)){
+            it.either(::onLoginFailed,::onLoginSucceeded)
+        }
+    }
+
+    fun resetPassword(email:String) {
+        if(state.value!!.resetPassword is Loading) {
+            setState { copy(showMsg = Event(stringProvider.getStringFromResource(R.string.please_wait))) }
+            return
+        }
+
+        if(!email.isValidEmail()){
+            setState { copy(showMsg = Event(stringProvider.getStringFromResource(R.string.invalid_email))) }
+            return
+        }
+
+        setState { copy(resetPassword = Loading()) }
+        scope.launchInteractor(resetPassword,email){
+            it.either(::handleResetPasswordFailure,::handleResetPasswordSucceeded)
+        }
+    }
+
+    fun finishResetPassword() {
+        setState { copy(resetPassword = null) }
+    }
+
+
+    private fun onLoginSucceeded(none: None) {
+        Timber.v("login succeeded")
+        setState { copy(login = Success(none)) }
+    }
+
+    private fun onLoginFailed(authFailure: AuthFailure) {
+        Timber.v("login succeeded $authFailure")
+        val erroMsg = when(authFailure){
+            AuthFailure.InternetConnection -> stringProvider.getStringFromResource(R.string.internet_prblm)
+            AuthFailure.InvalidEmail -> stringProvider.getStringFromResource(R.string.invalid_email)
+            AuthFailure.AuthFailed -> stringProvider.getStringFromResource(R.string.login_failure)
+            else -> stringProvider.getStringFromResource(R.string.unknown_error)
+        }
+        setState { copy(errorMsg = Event(erroMsg), login = Fail(authFailure)) }
+    }
+
+    private fun handleResetPasswordSucceeded(none: None) {
+        setState { copy(resetPassword = Success(none)) }
+    }
+
+    private fun handleResetPasswordFailure(authFailure: AuthFailure) {
+
+        val msg = when(authFailure){
+            AuthFailure.InternetConnection -> stringProvider.getStringFromResource(R.string.internet_prblm)
+            AuthFailure.InvalidEmail -> stringProvider.getStringFromResource(R.string.invalid_email)
+            else -> stringProvider.getStringFromResource(R.string.unknown_error)
+        }
+
+        setState { copy(resetPassword = Fail(authFailure),showMsg = Event(msg)) }
+    }
+}
+
+
+data class LoginState(
+    val login : Async<None>? = null,
+    val resetPassword : Async<None>? = null,
+    val errorMsg : Event<String>?=null,
+    val showMsg : Event<String>? = null
+) : State
