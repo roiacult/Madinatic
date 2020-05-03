08163a2fb445b20d468d44ca11567747844f541d
roiacult <roiacult27@gmail.com>
2020-05-03 16:18:27 +0100

    add databindings in profile xml

--- app/src/main/java/com/roacult/madinatic/utils/BindingAdapters.kt
+++ app/src/main/java/com/roacult/madinatic/utils/BindingAdapters.kt
@@ -1,23 +1,16 @@
 package com.roacult.madinatic.utils
 
-import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
-
-
-
-@BindingAdapter("app:srcCompat")
-fun ImageView.loadDrawable(drawable: Drawable){
-    setImageDrawable(drawable)
-}
 
 @BindingAdapter("showInt")
 fun TextView.showInt(int :Int){
     text = int.toString()
 }
 
-fun ImageView.loadImage(url:String){
-    Picasso.get().load(url).into(this)
+@BindingAdapter("loadImage")
+fun ImageView.loadImage(url:String?){
+    if(url != null && url.isNotEmpty() ) Picasso.get().load(url).into(this)
 }
