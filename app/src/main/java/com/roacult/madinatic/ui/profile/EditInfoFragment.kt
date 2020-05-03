1249455b3c22609fc6e4d42352d6d5efe1039c2d
roiacult <roiacult27@gmail.com>
2020-05-03 22:53:27 +0100

    create edit info fragment

---
+++ app/src/main/java/com/roacult/madinatic/ui/profile/EditInfoFragment.kt
@@ -0,0 +1,16 @@
+package com.roacult.madinatic.ui.profile
+
+import android.os.Bundle
+import android.view.View
+import com.roacult.madinatic.R
+import com.roacult.madinatic.base.FullScreenFragment
+import com.roacult.madinatic.databinding.ProfileChangeInfoBinding
+
+class EditInfoFragment : FullScreenFragment<ProfileChangeInfoBinding>() {
+    override val layutIdRes = R.layout.profile_change_info
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
--- app/src/main/java/com/roacult/madinatic/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/roacult/madinatic/ui/profile/ProfileFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import com.roacult.madinatic.R
 import com.roacult.madinatic.base.BaseFragment
 import com.roacult.madinatic.databinding.ProfileBinding
+import com.roacult.madinatic.utils.navigation.FragmentNavigation
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ProfileFragment : BaseFragment<ProfileBinding>() {
@@ -15,6 +16,9 @@ class ProfileFragment : BaseFragment<ProfileBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+
         viewModel.observe(this){
             it.errorMsg?.getContentIfNotHandled()?.let(::onError)
             it.clickEvent?.getContentIfNotHandled()?.let(::handlClickEvent)
@@ -22,10 +26,14 @@ class ProfileFragment : BaseFragment<ProfileBinding>() {
         }
     }
 
+    private fun initViews() {
+        binding.viewModel = viewModel
+    }
+
     private fun handlClickEvent(profileClickEvent: ProfileClickEvent) {
         when(profileClickEvent) {
             ProfileClickEvent.CHANGEINFO -> {
-                //TODO
+                vm.navigate(FragmentNavigation(destinationClass = EditInfoFragment::class.java))
             }
             ProfileClickEvent.CHANGEPASSWORD -> {
                 //TODO
