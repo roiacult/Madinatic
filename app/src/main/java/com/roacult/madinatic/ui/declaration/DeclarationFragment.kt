9e1869238b73b965a27cb2a9f57379292c1a8a55
roiacult <roiacult27@gmail.com>
2020-05-09 16:37:33 +0100

    navigate to add declaration

--- app/src/main/java/com/roacult/madinatic/ui/declaration/DeclarationFragment.kt
+++ app/src/main/java/com/roacult/madinatic/ui/declaration/DeclarationFragment.kt
@@ -1,9 +1,26 @@
 package com.roacult.madinatic.ui.declaration
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.get
 import com.roacult.madinatic.R
 import com.roacult.madinatic.base.BaseFragment
-import com.roacult.madinatic.databinding.UnderConstructionBinding
+import com.roacult.madinatic.databinding.DeclarationBinding
+import com.roacult.madinatic.ui.declaration.adddeclaration.AddDeclaration
+import com.roacult.madinatic.utils.navigation.FragmentNavigation
 
-class DeclarationFragment : BaseFragment<UnderConstructionBinding>() {
-    override val layutIdRes = R.layout.declration
+class DeclarationFragment : BaseFragment<DeclarationBinding>() {
+    override val layutIdRes = R.layout.declaration
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.toolbar.menu[0].setOnMenuItemClickListener {
+            vm.navigate(FragmentNavigation(destinationClass = AddDeclaration::class.java))
+            true
+        }
+    }
 }
---
+++ app/src/main/java/com/roacult/madinatic/ui/declaration/adddeclaration/AddDeclaration.kt
@@ -0,0 +1,9 @@
+package com.roacult.madinatic.ui.declaration.adddeclaration
+
+import com.roacult.madinatic.R
+import com.roacult.madinatic.base.FullScreenFragment
+import com.roacult.madinatic.databinding.AddDeclarationBinding
+
+class AddDeclaration : FullScreenFragment<AddDeclarationBinding>() {
+    override val layutIdRes = R.layout.add_declaration
+}
