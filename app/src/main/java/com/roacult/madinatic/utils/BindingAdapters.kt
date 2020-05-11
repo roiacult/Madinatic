81972c575fb9aeee82a61b5bd19d9b48eeca5083
roiacult <roiacult27@gmail.com>
2020-05-12 00:15:31 +0100

    create add product view v2

--- app/src/main/java/com/roacult/madinatic/utils/BindingAdapters.kt
+++ app/src/main/java/com/roacult/madinatic/utils/BindingAdapters.kt
@@ -1,9 +1,11 @@
 package com.roacult.madinatic.utils
 
+import android.net.Uri
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
+import java.io.File
 
 @BindingAdapter("showInt")
 fun TextView.showInt(int :Int){
@@ -13,4 +15,11 @@ fun TextView.showInt(int :Int){
 @BindingAdapter("loadImage")
 fun ImageView.loadImage(url:String?){
     if(url != null && url.isNotEmpty() ) Picasso.get().load(url).into(this)
+}
+
+@BindingAdapter("loadUri")
+fun ImageView.loadImageUri(path : String?) {
+    if(path != null) {
+        setImageURI(Uri.fromFile(File(path)))
+    }
 }
