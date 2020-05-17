4192d55927777ee2cd91f2fe2727965cf3683147
roiacult <roiacult27@gmail.com>
2020-05-17 16:38:50 +0100

    unfinished declaration fragment

---
+++ app/src/main/java/com/roacult/madinatic/ui/profile/unfinished/UnfinishedDeclarationFragment.kt
@@ -0,0 +1,17 @@
+package com.roacult.madinatic.ui.profile.unfinished
+
+import android.os.Bundle
+import android.view.View
+import com.roacult.madinatic.R
+import com.roacult.madinatic.base.FullScreenFragment
+import com.roacult.madinatic.databinding.AllDeclarationsBinding
+
+class UnfinishedDeclarationFragment : FullScreenFragment<AllDeclarationsBinding>() {
+
+    override val layutIdRes = R.layout.all_declarations
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
